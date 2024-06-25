edffd76b3de4f89c1e68e7caa9427e3dd3ab619e
Tiago <tiago.cambara@visualsoft.co.uk>
2024-06-25 19:32:07 +0100

    feat(sc-181): load the swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupHost } from './setup/host.loader';
+import { setupSwagger } from './setup/swagger.load';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  setupSwagger(app);
   await setupHost(app);
 }
 bootstrap();
